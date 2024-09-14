6ecab0161cc5ed4c3a9307635cc24d1e35867c0a
Ali <hoccalo@gmail.com>
2024-09-14 16:33:20 +0300

    menu and navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,29 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <h1>dashboard Layout</h1>
-      <div>{children}</div>
+    <div className="h-screen flex">
+      <div className="w-[10%]  lg:w-[16%] p-4">
+        <Link
+          href={"/"}
+          className="flex items-center  justify-center lg:justify-start gap-2 pt-2"
+        >
+          <Image src={"/logo.svg"} alt="logo" width={32} height={32} />
+          <span className="hidden xl:block">EducationCampus</span>
+        </Link>
+        <Menu />
+      </div>
+      <div className="w-[90%] md:w-[92%] lg:w-[84%]  bg-[#f7f8fa] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
